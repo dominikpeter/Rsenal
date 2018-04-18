e650d13b20b68ba1a3eba34c23b60f72ae88337f
dominik <dominikpeter@users.noreply.github.com>
2018-04-18 13:01:10 +0200

    Update filter-helper.R

--- R/filter-helper.R
+++ R/filter-helper.R
@@ -5,10 +5,23 @@
 
 
 #' @export
+`%notbetween%` <- function(x,y) !data.table::`%between%`(x, y)
+
+
+#' @export
 `%like%` <- data.table::`%like%`
+
+
+#' @export
+`%notlike%` <- function(x,y) !data.table::`%like%`(x, y)
+
 
 #' @export
 `%inrange%` <- data.table::`%inrange%`
+
+
+#' @export
+`%notinrange%` <- function(x,y) !data.table::`%inrange%`(x, y)
 
 
 #' @export
